fcf3d1187598676e792064150042b6a3e75b7de7
sumeetbidlani <149326477+thinkfeatSS@users.noreply.github.com>
2024-09-18 13:14:25 +0500

    curd poet

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
-const exampleController = require('../controllers/exampleController');
+const {getPoet,createPoet,updatePoet,getPoetById,deletePoet} = require('../controllers/poetController');
 
 const router = express.Router();
 
-router.get('/example', exampleController.getExample);
-router.post('/example', exampleController.postExample);
+// GET
+router.get('/poets',getPoet);
+router.get('/poets/:id',getPoetById); // Update example
+// POST
+router.post('/poets',createPoet);
+// PUT
+router.put('/poets/:id',updatePoet); // Update example
+// DELETE
+router.delete('/poets/:id',deletePoet); // Delete example
+
 
 module.exports = router;
